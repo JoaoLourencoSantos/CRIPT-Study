3ac002ecaadc6427ff00760106751c70894e7685
Joao Santos <lourencosjm41@gmail.com>
2019-05-12 02:47:51 -0300

    alteração

---
+++ check.php
@@ -0,0 +1,15 @@
+﻿<?php
+    $valor=$_POST['valor'];
+    if($valor=="abacate"){
+	echo"<center>";
+	echo "<h1>Aeeee, você conseguiu, meus parabéns!</h1>";
+	echo"</center>";
+	
+    }else{
+	echo"<center>";
+	echo "<h1>Shii, deu mole meu parceiro!</h1>";
+	echo"<h4><a href='index.html'>Tentar Novamente</a></h4>";
+	echo"</center>";
+    }
+
+?>
